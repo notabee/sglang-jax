0b3ba30eabe4096bee7c7433d91c7fb098def3e1
Rishabh Baghel <rishabhbaghel@google.com>
2026-05-09 13:21:36 +0000

    remove hacks

--- python/sgl_jax/srt/layers/attention/mla_backend.py
+++ python/sgl_jax/srt/layers/attention/mla_backend.py
@@ -339,10 +339,7 @@ class MLAAttentionBackend(AttentionBackend):
                 num_queries_per_block=self.num_queries_per_block,
                 decode_batch_size=self.decode_batch_size,
                 vmem_limit_bytes=self.vmem_limit_bytes,
-                debug_mode=False,
             )
-
-
 
         o_latent, updated_cache = jax.shard_map(
             _run,
