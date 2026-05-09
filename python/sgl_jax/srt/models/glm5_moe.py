d36c4996858f17762d565f815e3d03c26493c411
Rishabh Baghel <rishabhbaghel@google.com>
2026-05-09 12:12:32 +0000

    C

--- python/sgl_jax/srt/models/glm5_moe.py
+++ python/sgl_jax/srt/models/glm5_moe.py
@@ -352,7 +352,7 @@ class Glm5Attention(nnx.Module):
         k_rope = jnp.broadcast_to(
             k_rope,
             (k_rope.shape[0], self.num_heads, self.qk_rope_head_dim),
-            out_sharding=P(None, "tensor", None),
+            out_sharding=P("data", "tensor", None),
         )
 
         q = jnp.concatenate([q_nope, q_rope], axis=-1)
